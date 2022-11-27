226bd56639c7fae957890c45f2213121127394c6
Harsh Verma <harsh.verma@botmantra.com>
2022-11-27 14:54:42 +0530

    chnage title

--- src/CustomUploadImage.tsx
+++ src/CustomUploadImage.tsx
@@ -1,3 +1,25 @@
+import { styled } from "@mui/material";
+
+const StyledUploadFileInput = styled("div")(() => ({
+  " & input[type='file']": {
+    visibility: "hidden",
+    width: "1px",
+  },
+  " & div": {
+    backgroundColor: "#ddd",
+    borderColor: "#ccc",
+    color: "#333",
+    padding: "3px 0",
+    borderRadius: "5px",
+    cursor: "pointer",
+    width: "195px",
+    textAlign: "center",
+  },
+  "& label": {
+    cursor: "pointer",
+    padding: "3px 53px",
+  },
+}));
 export default function CustomUploadImage({
   children,
   value,
@@ -7,32 +29,47 @@ export default function CustomUploadImage({
   ref,
 }: any) {
   return (
-    <label
-      htmlFor="contained-button-file"
-      className="m-0 w-100"
-      style={{
-        display: "flex",
-        border: "1px solid black",
-        width: "300px",
-        justifyContent: "center",
-        padding: "10px",
-        margin: "10px",
-        marginLeft: "0",
-        marginRight: "0",
-      }}
-    >
+    // <label
+    //   htmlFor="contained-button-file"
+    //   className="m-0 w-100"
+    //   style={{
+    //     display: "flex",
+    //     border: "1px solid black",
+    //     width: "300px",
+    //     justifyContent: "center",
+    //     padding: "10px",
+    //     margin: "10px",
+    //     marginLeft: "0",
+    //     marginRight: "0",
+    //   }}
+    // >
+    //   <input
+    //     value={value}
+    //     accept={accept}
+    //     disabled={disabled}
+    //     ref={ref}
+    //     style={{ display: "none" }}
+    //     id="contained-button-file"
+    //     type="file"
+    //     onChange={(e) => onChange(e)}
+    //     onDrop={e=>onChange(e)}
+    //   />
+    //   {children}
+    // </label>
+
+    <StyledUploadFileInput>
+    <div>
+      <label htmlFor="file">Select a file</label>
       <input
-        value={value}
-        accept={accept}
-        disabled={disabled}
-        ref={ref}
-        style={{ display: "none" }}
-        id="contained-button-file"
         type="file"
-        onChange={(e) => onChange(e)}
-        onDrop={e=>onChange(e)}
+        onChange={(e) => {
+          onChange(e);
+        }}
+        id="file"
+        accept="application/pdf"
       />
-      {children}
-    </label>
+    </div>
+  
+  </StyledUploadFileInput>
   );
 }
